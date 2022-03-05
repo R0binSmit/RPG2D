ccb63c7844298d846f53110658b34139e55224ee
BuildTools <unconfigured@null.spigotmc.org>
2022-03-05 12:39:04 +0100

    Add project files.

---
+++ RPG2D/Game1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RPG2D_Windows
+{
+    internal class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        public Game1(): base() 
+        {
+            graphics = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+        }
+    }
+}
---
+++ RPG2D/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace RPG2D_Windows
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (Game1 game = new Game1()) 
+            {
+                game.Run();
+            }
+        }
+    }
+}
