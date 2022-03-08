98f3f819ec32db2189ae643ea701af71ae0bcb8b
BuildTools <unconfigured@null.spigotmc.org>
2022-03-08 21:04:40 +0100

    restructure player image handling.

--- RPG2D/Game1.cs
+++ RPG2D/Game1.cs
@@ -6,6 +6,8 @@ namespace RPG2D_Windows
     internal class Game1 : Game
     {
         private GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private Player player;
         public Game1(): base() 
         {
             graphics = new GraphicsDeviceManager(this);
@@ -19,11 +21,16 @@ namespace RPG2D_Windows
         protected override void LoadContent()
         {
             base.LoadContent();
+
+            /* Load Player */
+            player = new Player();
+            player.Texture2D = Content.Load<Texture2D>("content/player");
         }
 
         protected override void UnloadContent()
         {
             base.UnloadContent();
+            Content.Unload();
         }
 
         protected override void Update(GameTime gameTime)
@@ -33,8 +40,10 @@ namespace RPG2D_Windows
 
         protected override void Draw(GameTime gameTime)
         {
-            base.Draw(gameTime);
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            player.Draw();
+
+            base.Draw(gameTime);
         }
     }
 }
--- RPG2D/Player.cs
+++ RPG2D/Player.cs
@@ -1,10 +1,26 @@
-﻿namespace RPG2D_Windows
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RPG2D_Windows
 {
     internal class Player
     {
+        public Texture2D Texture2D { get; set; }
+        public SpriteBatch SpriteBatch { get; set; }
+        private Vector2 vector2D { get; set; }
         public Player()
         {
-            
+            vector2D = new Vector2(0, 0);
+        }
+
+        public void Draw() 
+        {
+            if(SpriteBatch != null)
+            {
+                SpriteBatch.Begin();
+                SpriteBatch.Draw(Texture2D, Vector2.Zero, Color.Black);
+                SpriteBatch.End();
+            }
         }
     }
 }
